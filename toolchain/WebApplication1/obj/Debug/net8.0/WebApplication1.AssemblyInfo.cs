fd9a5eaf955ade7d811f8bbc9bae2169219b544d
Emufr3ak <emufr3ak@icloud.com>
2024-10-06 10:46:34 +0200

    added cstpastechar

--- toolchain/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ toolchain/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93f528f3fc2eff24228105c9183af84ef4e3a36e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90872fa09ae61c3c0314dc5526ee0acaf97097b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
